e24bf28e5412fd2c678524264f0001f6b449a0f6
Pc <leitemateus819@gmail.com>
2025-11-28 13:04:02 -0300

    Criado a controller

---
+++ src/main/java/prog/teus/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package prog.teus.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+
+    @GetMapping("/boasvindas")
+    public String boasVindas(){
+        return "Essa e minha primeira mensagem nessa rota!";
+    }
+}
