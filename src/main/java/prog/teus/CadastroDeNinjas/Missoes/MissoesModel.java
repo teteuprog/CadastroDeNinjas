c3c8d223bbc10e6285188243a6b2bc1f19f9a1f2
Pc <leitemateus819@gmail.com>
2025-12-03 12:51:41 -0300

    Aplicanco o Lombok com as anotations: @Data, @AllArgsConstructor e @NoArgsConstructor.

--- src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,6 +1,9 @@
 package prog.teus.CadastroDeNinjas.Missoes;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import prog.teus.CadastroDeNinjas.Ninjas.NinjaModel;
 
 import java.util.LinkedList;
@@ -8,38 +11,18 @@ import java.util.LinkedList;
 
 @Entity
 @Table(name = "tb_cadastro_missoes")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class MissoesModel {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome, rank;
 
     @OneToMany(mappedBy = "missoes")
     private LinkedList<NinjaModel> ninja;
 
-    public MissoesModel() {
-    }
-
-    public MissoesModel(String nomeDaMissao, String rankMissao) {
-        this.nome = nomeDaMissao;
-        this.rank = rankMissao;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getRank() {
-        return rank;
-    }
-
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
 }
--- src/main/java/prog/teus/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/prog/teus/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,53 +1,28 @@
 package prog.teus.CadastroDeNinjas.Ninjas;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import prog.teus.CadastroDeNinjas.Missoes.MissoesModel;
 
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class NinjaModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String nome, email;
+
     private int idade;
 
     @ManyToOne
     @JoinColumn(name = "missoes_id")
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
 
