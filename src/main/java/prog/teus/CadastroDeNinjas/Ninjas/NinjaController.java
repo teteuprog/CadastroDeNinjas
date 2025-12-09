6a56ee14e8d8d16177edc1bc26b9210689ed3336
Pc <leitemateus819@gmail.com>
2025-12-09 14:09:09 -0300

    feat: atualiza o Controller com mock de dados

--- src/main/java/prog/teus/CadastroDeNinjas/Ninjas/NinjaController.java
+++ src/main/java/prog/teus/CadastroDeNinjas/Ninjas/NinjaController.java
@@ -1,8 +1,6 @@
 package prog.teus.CadastroDeNinjas.Ninjas;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
@@ -11,5 +9,29 @@ public class NinjaController {
     @GetMapping("/boasvindas")
     public String boasVindas(){
         return "Essa e minha primeira mensagem nessa rota!";
+    }
+
+    //Cria um novo Ninja
+    @PostMapping("/criarNinja")
+    public String criarNinja(){
+        return "Ninja criado";
+    }
+
+    //Altera um ninja por IDs
+    @PutMapping("/alterId")
+    public String alterarPorId(){
+        return "Ninja alterado por Id";
+    }
+
+    //Mostra todos os Ninjas
+    @GetMapping("/all")
+    public String mostrarTodoOsNinjas(){
+        return "Todos os ninjas";
+    }
+
+    //Deleta o ninja por ID
+    @DeleteMapping("/deletar")
+    public String deletarNinjaPorId(){
+        return "Ninja deletado por id";
     }
 }
