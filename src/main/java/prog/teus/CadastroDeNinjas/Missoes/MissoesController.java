baa4714005a5e5d96e6c03fd695f69ccaa79a66d
Pc <leitemateus819@gmail.com>
2025-12-09 14:27:59 -0300

    feat: Criar as rotas do controller com mock

--- src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,4 +1,32 @@
 package prog.teus.CadastroDeNinjas.Missoes;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("missoes")
 public class MissoesController {
+
+    //Lista as missoes
+    @GetMapping("/listar")
+    public String listarMissoes(){
+        return"Missoes listadas com sucesso";
+    }
+
+    //Cria missoes
+    @PostMapping("/criar")
+    public String criarMissoes(){
+        return "Missoes criadas com sucesso";
+    }
+
+    //Altera as missoes
+    @PutMapping("/alterar")
+    public String alterarMissoes(){
+        return "Missoes alteradas com sucesso";
+    }
+
+    //Deleta as missoes
+    @DeleteMapping("deletar")
+    public String deletarMissoes(){
+        return "Missoes deletadas com sucesso";
+    }
 }
