56b0d07d7b8f25e7b75ea8ee891ada1ddb442529
Pc <leitemateus819@gmail.com>
2025-12-10 01:02:07 -0300

    feat: atualiza o service de missoes implementando o CRUD

--- src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesService.java
+++ src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesService.java
@@ -1,4 +1,37 @@
 package prog.teus.CadastroDeNinjas.Missoes;
 
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class MissoesService {
+
+    private MissoesRepository missoesRepository;
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    //Listar todos os ninjas
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+    //Lista por id
+    public MissoesModel listarPorId(Long id){
+        Optional<MissoesModel> listarporid = missoesRepository.findById(id);
+        return listarporid.orElse(null);
+    }
+
+    //Cria missoes
+    public MissoesModel criarMissoes(MissoesModel missoesModel){
+        return missoesModel = missoesRepository.save(missoesModel);
+    }
+
+    //Deleta missoes
+    public void deletarMissao(Long id){
+        missoesRepository.deleteById(id);
+    }
 }
