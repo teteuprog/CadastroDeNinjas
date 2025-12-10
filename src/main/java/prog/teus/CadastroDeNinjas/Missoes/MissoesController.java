4dfb584c668d6eeb222f39ecefcb13322e358865
Pc <leitemateus819@gmail.com>
2025-12-10 00:56:02 -0300

    feat: Adiciona mock de endpoint na controller de missoes

--- src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesController.java
+++ src/main/java/prog/teus/CadastroDeNinjas/Missoes/MissoesController.java
@@ -1,21 +1,35 @@
 package prog.teus.CadastroDeNinjas.Missoes;
 
 import org.springframework.web.bind.annotation.*;
+import prog.teus.CadastroDeNinjas.Ninjas.NinjaModel;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/missoes")
 public class MissoesController {
 
+    private MissoesService missoesService;
+
+    public MissoesController(MissoesService missoesService) {
+        this.missoesService = missoesService;
+    }
+
     //Lista as missoes
     @GetMapping("/listar")
-    public String listarMissoes(){
-        return"Missoes listadas com sucesso";
+    public List<MissoesModel> listarMissoes(){
+        return missoesService.listarMissoes();
+    }
+    //Listar por id
+    @GetMapping("/listar/{id}")
+    public MissoesModel listarPorId(@PathVariable Long id){
+        return missoesService.listarPorId(id);
     }
 
     //Cria missoes
     @PostMapping("/criar")
-    public String criarMissoes(){
-        return "Missoes criadas com sucesso";
+    public MissoesModel criarMissoes(@RequestBody MissoesModel missao){
+        return missoesService.criarMissoes(missao);
     }
 
     //Altera as missoes
@@ -25,8 +39,8 @@ public class MissoesController {
     }
 
     //Deleta as missoes
-    @DeleteMapping("deletar")
-    public String deletarMissoes(){
-        return "Missoes deletadas com sucesso";
+    @DeleteMapping("deletar/{id}")
+    public void deletarMissoes(@PathVariable Long id){
+        missoesService.deletarMissao(id);
     }
 }
